6e9b556730b67b56f483ac4391e8ce58191f399b
ZzZEN <johnnykatana1@gmail.com>
2025-10-13 04:06:22 +0200

    Add Makefile and initial logic

--- Makefile
+++ Makefile
@@ -1,1 +1,3 @@
-gcc -o algo_vis main.c -Wextra -Wall -I include -L lib -lraylib -lgdi32 -lwinmm
+PROTOTYPE_EXECUTABLE: gcc -o algo_vis main.c -Wextra -Wall -I include -L lib -lraylib -lgdi32 -lwinmm
+GCC_FLAGS: -Wextra -Wall 
+INCLUDES: -I include -L lib -lraylib -lgdi32 -lwinmm
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include <stdint.h>
 #include <raylib.h>
 
 /*#include "./states/menuState/menuState.h"
@@ -8,9 +9,23 @@
 #include "./states/playState/playState.h"
 */
 
+/* some global constants for initalization*/
+
+// actual initialized window
+const int32_t SCREEN_WIDTH = 1280;
+const int32_t SCREEN_HEIGHT = 720;
+
+// virtual size inside window (scalable)
+/* const int32_t VIRTUAL_WIDTH = 480; 
+const int32_t VIRTUAL_HEIGHT = 320;
+
+to be implemented
+*/
+
+
 int main(void)
 {
-    InitWindow(1280, 920, "Algorithms Interactive Visualiser");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Algorithms Interactive Visualiser");
     // while ESC key not pressed
     while (!WindowShouldClose())
     {
